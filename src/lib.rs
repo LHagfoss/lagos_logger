b298ec676f3b9855c7018491d8b17dc73cb189a9
LHagfoss <lucas@paral.no>
2026-05-19 13:30:04 +0200

    fixing fomratting and colors

--- src/lib.rs
+++ src/lib.rs
@@ -2,49 +2,40 @@
 pub use chrono::Local;
 pub use colored::*;
 
-// MACRO WITH TIME (Feature enabled)
+pub enum Level {
+    Info,
+    Warn,
+    Error,
+    Running,
+    Success,
+}
+
 #[cfg(feature = "time")]
 #[macro_export]
 macro_rules! logger {
-    (Info, $($arg:tt)*) => {{
-        let time = $crate::Local::now().format("%H:%M:%S");
-        println!("{} {:>7} {}", format!("{}", time).dimmed(), "INFO".green().bold(), format!($($arg)*))
-    }};
-    (Warn, $($arg:tt)*) => {{
-        let time = $crate::Local::now().format("%H:%M:%S");
-        println!("{} {:>7} {}", format!("{}", time).dimmed(), "WARN".yellow().bold(), format!($($arg)*))
-    }};
-    (Error, $($arg:tt)*) => {{
-        let time = $crate::Local::now().format("%H:%M:%S");
-        eprintln!("{} {:>7} {}", format!("{}", time).dimmed(), "ERROR".red().bold(), format!($($arg)*))
-    }};
-    (Running, $($arg:tt)*) => {{
-        let time = $crate::Local::now().format("%H:%M:%S");
-        println!("{} {:>7} {}", format!("{}", time).dimmed(), "RUNNING".green().bold(), format!($($arg)*))
-    }};
-    (Success, $($arg:tt)*) => {{
-        let time = $crate::Local::now().format("%H:%M:%S");
-        println!("{} {:>7} {}", format!("{}", time).dimmed(), "SUCCESS".green().bold(), format!($($arg)*))
+    ($level:expr, $($arg:tt)*) => {{
+        let time = $crate::Local::now().format("%Y-%m-%d %H:%M:%S");
+
+        match $level {
+            $crate::Level::Info => println!("{} {} {}", "Info".bright_cyan().bold(), time, format_args!($($arg)*)),
+            $crate::Level::Warn => println!("{} {} {}", "Warn".bright_yellow().bold(), time, format_args!($($arg)*)),
+            $crate::Level::Error => eprintln!("{} {} {}", "Error".bright_red().bold(), time, format_args!($($arg)*)),
+            $crate::Level::Running => println!("{} {} {}", "Running".bright_green().bold(), time, format_args!($($arg)*)),
+            $crate::Level::Success => println!("{} {} {}", "Success".bright_green().bold(), time, format_args!($($arg)*)),
+        }
     }};
 }
 
-// MACRO WITHOUT TIME (Feature disabled)
 #[cfg(not(feature = "time"))]
 #[macro_export]
 macro_rules! logger {
-    (Info, $($arg:tt)*) => {{
-        println!("{:>7} {}", "INFO".green().bold(), format!($($arg)*))
-    }};
-    (Warn, $($arg:tt)*) => {{
-        println!("{:>7} {}", "WARN".yellow().bold(), format!($($arg)*))
-    }};
-    (Error, $($arg:tt)*) => {{
-        eprintln!("{:>7} {}", "ERROR".red().bold(), format!($($arg)*))
-    }};
-    (Running, $($arg:tt)*) => {{
-        println!("{:>7} {}", "RUNNING".green().bold(), format!($($arg)*))
-    }};
-    (Success, $($arg:tt)*) => {{
-        println!("{:>7} {}", "SUCCESS".green().bold(), format!($($arg)*))
+    ($level:expr, $($arg:tt)*) => {{
+        match $level {
+            $crate::Level::Info => println!("{:>7} {}", "Info".bright_cyan().bold(), format_args!($($arg)*)),
+            $crate::Level::Warn => println!("{:>7} {}", "Warn".bright_yellow().bold(), format_args!($($arg)*)),
+            $crate::Level::Error => eprintln!("{:>7} {}", "Error".bright_red().bold(), format_args!($($arg)*)),
+            $crate::Level::Running => println!("{:>7} {}", "Running".bright_green().bold(), format_args!($($arg)*)),
+            $crate::Level::Success => println!("{:>7} {}", "Success".bright_green().bold(), format_args!($($arg)*)),
+        }
     }};
 }
