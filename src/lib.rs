63549216cf8d5cd2011801bae08444f5cb4f74c3
LHagfoss <lucas@paral.no>
2026-05-19 12:44:09 +0200

    small warning fix

--- src/lib.rs
+++ src/lib.rs
@@ -2,50 +2,57 @@
 pub use chrono::Local;
 pub use colored::*;
 
-/// Pretty print macro that prints a info message with a green "INFO" prefix.
+// =========================================
+// MACROS WITH time (feature enabled)
+// =========================================
+#[cfg(feature = "time")]
 #[macro_export]
 macro_rules! info {
     ($($arg:tt)*) => {{
-        #[cfg(feature = "time")]
-        {
-            let current_time = $crate::Local::now().format("%H:%M:%S");
-            println!("{} {:>5} {}", format!("{}", current_time).dimmed(), format!("INFO").green().bold(), format!($($arg)*))
-        }
-        #[cfg(not(feature = "time"))]
-        {
-            println!("{:>5} {}", format!("INFO").green().bold(), format!($($arg)*))
-        }
+        let current_time = $crate::Local::now().format("%H:%M:%S");
+        println!("{} {:>5} {}", format!("{}", current_time).dimmed(), format!("INFO").green().bold(), format!($($arg)*))
     }};
 }
 
-/// Pretty print macro that prints a warning message with a yellow "WARN" prefix.
+#[cfg(feature = "time")]
 #[macro_export]
 macro_rules! warn {
     ($($arg:tt)*) => {{
-        #[cfg(feature = "time")]
-        {
-            let current_time = $crate::Local::now().format("%H:%M:%S");
-            println!("{} {:>5} {}", format!("{}", current_time).dimmed(), format!("WARN").yellow().bold(), format!($($arg)*))
-        }
-        #[cfg(not(feature = "time"))]
-        {
-            println!("{:>5} {}", format!("WARN").yellow().bold(), format!($($arg)*))
-        }
+        let current_time = $crate::Local::now().format("%H:%M:%S");
+        println!("{} {:>5} {}", format!("{}", current_time).dimmed(), format!("WARN").yellow().bold(), format!($($arg)*))
     }};
 }
 
-/// Pretty print macro that prints an error message with a red "ERROR" prefix.
+#[cfg(feature = "time")]
 #[macro_export]
 macro_rules! error {
     ($($arg:tt)*) => {{
-        #[cfg(feature = "time")]
-        {
-            let current_time = $crate::Local::now().format("%H:%M:%S");
-            eprintln!("{} {:>5} {}", format!("{}", current_time).dimmed(), format!("ERROR").red().bold(), format!($($arg)*))
-        }
-        #[cfg(not(feature = "time"))]
-        {
-            eprintln!("{:>5} {}", format!("ERROR").red().bold(), format!($($arg)*))
-        }
+        let current_time = $crate::Local::now().format("%H:%M:%S");
+        eprintln!("{} {:>5} {}", format!("{}", current_time).dimmed(), format!("ERROR").red().bold(), format!($($arg)*))
+    }};
+}
+
+// MACROS WITHOUT TIME (feature disabled)
+#[cfg(not(feature = "time"))]
+#[macro_export]
+macro_rules! info {
+    ($($arg:tt)*) => {{
+        println!("{:>5} {}", format!("INFO").green().bold(), format!($($arg)*))
+    }};
+}
+
+#[cfg(not(feature = "time"))]
+#[macro_export]
+macro_rules! warn {
+    ($($arg:tt)*) => {{
+        println!("{:>5} {}", format!("WARN").yellow().bold(), format!($($arg)*))
+    }};
+}
+
+#[cfg(not(feature = "time"))]
+#[macro_export]
+macro_rules! error {
+    ($($arg:tt)*) => {{
+        eprintln!("{:>5} {}", format!("ERROR").red().bold(), format!($($arg)*))
     }};
 }
